bc265e52b39a81c8d78cc7a29093cccce9d740dd
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-30 18:19:08 -0800

    added cors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express"); 
-const cors = require("cors"); 
 const helmet = require("helmet"); 
+const cors = require("cors"); 
 
 
 
@@ -13,14 +13,11 @@ const ScoresRouter = require("./routes/scores")
 
 const server = express(); 
 
-server.use(cors());
+
 server.use(helmet());  
+server.use(cors()); 
 server.use(express.json()); 
-server.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
-    next(); 
-})
+
 
 
 server.use("/api", UserRouter)
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -209,7 +209,7 @@ const Register = ({username, email, password, handleInputChange}) => {
 
       const newUser = { username, email, password }; 
 
-      const response = await axios.post("http://prog-typing.herokuapp.com/api/register", newUser); 
+      const response = await axios.post("http://localhost:5000/api/register", newUser); 
 
       console.log(response); 
 
