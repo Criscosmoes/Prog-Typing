a47d4576842e5b2e2d09185985c4b9b4f805bd13
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-31 10:58:07 -0800

    second middleware

--- api/server.js
+++ api/server.js
@@ -15,9 +15,21 @@ const server = express();
 
 
 server.use((req, res, next) => {
-    req.header("Access-Control-Allow-Origin","http://localhost:3000/")
-    req.header("Access-Control-Allow-Methods", "POST"); 
-    req.header("Access-Control-Allow-Headers", "Content-Type"); 
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
 })
 server.use(cors()); 
 server.use(helmet());  
