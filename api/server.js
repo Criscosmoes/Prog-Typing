2767a844e53f09576b4d48d4e7b64f9f65e2fb72
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-12-31 17:43:58 -0800

    commented out cors

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const ScoresRouter = require("./routes/scores")
 const server = express(); 
 
 
-server.use((req, res, next) => {
+/* server.use((req, res, next) => {
    // Website you wish to allow to connect
    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 
@@ -23,13 +23,9 @@ server.use((req, res, next) => {
    // Request headers you wish to allow
    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-   // Set to true if you need the website to include cookies in the requests sent
-   // to the API (e.g. in case you use sessions)
-   res.header('Access-Control-Allow-Credentials', true);
-
    // Pass to next layer of middleware
    next();
-})
+}) */
 server.use(cors()); 
 server.use(helmet());  
 server.use(express.json()); 
